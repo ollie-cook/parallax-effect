5281f0f9c0af28092b6051294b0e829b12a8b97e
Oliver Cook <olliewatford@gmail.com>
2024-02-12 19:53:43 +0000

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,101 @@
-import Image from "next/image";
+'use client'
+
+import { useState, useEffect } from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import Lottie from "lottie-react"
+import animationData from "@/public/fire-animation.json"
+import dustAnimation from "@/public/dust-animation.json"
+import { IoIosArrowDown } from "react-icons/io";
+import "./page.css"
 
 export default function Home() {
+  const [isWobble, setIsWobble] = useState(false); 
+  const [isDust, setIsDust] = useState(false);
+  
+
+  useEffect(() => {
+    const checkScroll = () => {
+      const scrollableElement = document.querySelector('#scrollable-element') as HTMLElement;
+      const scrollPosition = scrollableElement ? scrollableElement.scrollTop : 0;
+      const divHeight = scrollableElement ? scrollableElement.offsetHeight : 0;
+
+      if (scrollPosition > divHeight*3 && scrollPosition < divHeight*4.99) {
+        setIsWobble(true);
+      } else {
+        setIsWobble(false);
+      }
+
+      if (scrollPosition == divHeight*5) {
+        setIsDust(true);
+      } else {
+        setIsDust(false);
+      }
+    };
+
+    document.querySelector('#scrollable-element')?.addEventListener('scroll', checkScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.querySelector('#scrollable-element')?.removeEventListener('scroll', checkScroll);
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="">
+      <Parallax pages={6} id="scrollable-element">
+        <ParallaxLayer offset={0} factor={2} style={{backgroundImage: "linear-gradient(to bottom, rgba(255, 255, 255, 0), #181d2b), url(/nightsky.jpg)" ,backgroundSize: 'cover'}} />
+        <ParallaxLayer offset={2} factor={3} className="bg-gradient-to-b from-[#181d2b] to-[#55759b]" />
+        <ParallaxLayer offset={5} factor={1} style={{backgroundImage: "linear-gradient(to bottom, #55759b, rgba(255, 255, 255, 0)), url(/desert.jpg)" ,backgroundSize: 'cover'}} />
+        <ParallaxLayer offset={0} className="flex flex-col items-center" sticky={{ start: 0, end: 0.3 }}>
+          <img src="/spacex-logo.png" className="w-2/3 mt-16" />
+          <h2 className="text-white text-6xl mt-16">our rockets land.</h2>
+          <p className="mt-4 text-white">scroll for a demo</p>
+          <IoIosArrowDown className="h-8 w-8" style={{color: 'white'}}/>
+        </ParallaxLayer>
+        <ParallaxLayer className=" relative" sticky={{ start: 0.7, end: 5.2 }}>
+          <div className={`${isWobble == true ? 'spaceship-wobble' : ''} absolute left-1/2 -translate-x-1/2 top-[10%] h-[90%] flex flex-col items-center`}>
+            <img src="starship.png" className="h-5/6"></img>
+            {
+              isWobble == true &&
+              <Lottie 
+                animationData={animationData}
+                loop={true}
+                className="w-36 rotate-180 relative -top-4"
+              />
+            }
+            {
+              isDust == true &&
+              <Lottie 
+                animationData={dustAnimation}
+                loop={false}
+                className="w-64 relative -top-28 right-2"
+              />
+            }
+              
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={2}>
+          <img src="/satellite.png"  className="w-1/12 absolute left-[5%]"></img>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={0.1}>
+          <img src="/satellite.png"  className="w-[3%] absolute left-[25%] top-1/2"></img>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={2}>
+          <img src="/satellite.png"  className="w-1/12 absolute right-[15%] top-1/4"></img>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={0.1}>
+          <img src="/satellite.png"  className="w-[3%] absolute right-[25%] top-3/4"></img>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={1}>
+          <img src="/ufo.webp"  className="w-[5%] absolute right-[35%] top-1/2"></img>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={1}>
+          <img src="/ufo.webp"  className="w-[5%] absolute left-[30%] top-[95%]"></img>
+        </ParallaxLayer>
+        <ParallaxLayer className="relative" offset={2} speed={1.5}>
+          <img src="/roadster.png"  className=" w-[15%] absolute left-[8%] top-[55%]"></img>
+        </ParallaxLayer>
+      </Parallax>
     </main>
   );
 }
